27afa73a373b95a97e8a9c8ad7975f1bc0ace84f
Ryan Tan <Ryan_Tan_1998@hotmail.sg>
2021-07-28 13:31:11 +0800

    added basic footer

--- savevaccinesg/src/App.js
+++ savevaccinesg/src/App.js
@@ -10,19 +10,24 @@ import SubmitPage from './pages/submitPage'
 import LostPage from './pages/lostPage';
 
 
+
+
 function App() {
   return (
     <div className="App">
         <Router>
           <Switch>
             <Route exact path="/" component={LoginPage}/>
+            
             <Route exact path="/register" component={RegisterPage}/>
             <Route exact path="/dashboard/:id" component={Dashboard}/>
             <Route exact path="/request" component={RequestPage}/>
             <Route exact path="/submit" component={SubmitPage}/>
             <Route path="/404" component={LostPage}/>
             <Redirect to="/404"/>
+            
           </Switch>
+          
         </Router>
     </div>
   );
