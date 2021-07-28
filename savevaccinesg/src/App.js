925c919cc224b6d2d287cf403dcd0f508f5db4ab
Ryan Tan <Ryan_Tan_1998@hotmail.sg>
2021-07-28 12:00:15 +0800

    added all needed pages and components, router dom initialized.

--- savevaccinesg/src/App.js
+++ savevaccinesg/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+
+//IMPORT PAGES
+import LoginPage from './pages/loginPage'
+import RegisterPage from './pages/registerPage'
+import Dashboard from './pages/dashboardPage'
+import RequestPage from './pages/requestPage'
+import SubmitPage from './pages/submitPage'
+import LostPage from './pages/lostPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LoginPage}/>
+            <Route exact path="/register" component={RegisterPage}/>
+            <Route exact path="/dashboard/:id" component={Dashboard}/>
+            <Route exact path="/request" component={RequestPage}/>
+            <Route exact path="/submit" component={SubmitPage}/>
+            <Route path="/404" component={LostPage}/>
+            <Redirect to="/404"/>
+          </Switch>
+        </Router>
     </div>
   );
 }
