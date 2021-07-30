ba563cd224f74b62615569e0379f98a816d0581c
Ryan Tan <Ryan_Tan_1998@hotmail.sg>
2021-07-30 09:23:32 +0800

    added new dashboard items

--- savevaccinesg/src/pages/dashboardPage.js
+++ savevaccinesg/src/pages/dashboardPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Row, Col, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
@@ -7,107 +7,122 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 import MainFooter from '../commons/footer'
 import MainNavbar from '../commons/navbar'
 
-//IMPORT IMAGES
-import FakeChart from '../images/fakeChart.jpg'
 
 
 
 export default function Dashboard() {
-    
+
+    const [doses1, setDoses1] = useState()
+    const [doses2, setDoses2] = useState()
+    const [doses3, setDoses3] = useState()
+    const [doses4, setDoses4] = useState()
+
+    const UN = localStorage.getItem("params")
+
     const data = [
-        { 
-            name: '1 July', Cases: 319, pv: 100, amt: 100 
+        {
+            name: '1 July', Cases: 319, pv: 100, amt: 100
         },
-        { 
-            name: '2 July', Cases: 313, pv: 100, amt: 100 
+        {
+            name: '2 July', Cases: 313, pv: 100, amt: 100
         },
-        { 
-            name: '3 July', Cases: 305, pv: 100, amt: 100 
+        {
+            name: '3 July', Cases: 305, pv: 100, amt: 100
         },
-        { 
-            name: '4 July', Cases: 295, pv: 100, amt: 100 
+        {
+            name: '4 July', Cases: 295, pv: 100, amt: 100
         },
-        { 
-            name: '5 July', Cases: 295, pv: 100, amt: 100 
+        {
+            name: '5 July', Cases: 295, pv: 100, amt: 100
         },
-        { 
-            name: '6 July', Cases: 291, pv: 100, amt: 100 
+        {
+            name: '6 July', Cases: 291, pv: 100, amt: 100
         },
-        { 
-            name: '7 July', Cases: 275, pv: 100, amt: 100 
+        {
+            name: '7 July', Cases: 275, pv: 100, amt: 100
         },
-        { 
-            name: '8 July', Cases: 269, pv: 100, amt: 100 
+        {
+            name: '8 July', Cases: 269, pv: 100, amt: 100
         },
-        { 
-            name: '9 July', Cases: 268, pv: 100, amt: 100 
+        {
+            name: '9 July', Cases: 268, pv: 100, amt: 100
         },
-        { 
-            name: '10 July', Cases: 251, pv: 100, amt: 100 
+        {
+            name: '10 July', Cases: 251, pv: 100, amt: 100
         },
-        { 
-            name: '11 July', Cases: 242, pv: 100, amt: 100 
+        {
+            name: '11 July', Cases: 242, pv: 100, amt: 100
         },
-        { 
-            name: '12 July', Cases: 250, pv: 100, amt: 100 
+        {
+            name: '12 July', Cases: 250, pv: 100, amt: 100
         },
-        { 
-            name: '13 July', Cases: 255, pv: 100, amt: 100 
+        {
+            name: '13 July', Cases: 255, pv: 100, amt: 100
         },
-        { 
-            name: '14 July', Cases: 301, pv: 100, amt: 100 
+        {
+            name: '14 July', Cases: 301, pv: 100, amt: 100
         },
-        { 
-            name: '15 July', Cases: 335, pv: 100, amt: 100 
+        {
+            name: '15 July', Cases: 335, pv: 100, amt: 100
         },
-        { 
-            name: '16 July', Cases: 379, pv: 100, amt: 100 
+        {
+            name: '16 July', Cases: 379, pv: 100, amt: 100
         },
-        { 
-            name: '17 July', Cases: 433, pv: 100, amt: 100 
+        {
+            name: '17 July', Cases: 433, pv: 100, amt: 100
         },
-        { 
-            name: '18 July', Cases: 511, pv: 100, amt: 100 
+        {
+            name: '18 July', Cases: 511, pv: 100, amt: 100
         },
-        { 
-            name: '19 July', Cases: 683, pv: 100, amt: 100 
+        {
+            name: '19 July', Cases: 683, pv: 100, amt: 100
         },
-        { 
-            name: '20 July', Cases: 861, pv: 100, amt: 100 
+        {
+            name: '20 July', Cases: 861, pv: 100, amt: 100
         },
-        { 
-            name: '21 July', Cases: 1025, pv: 100, amt: 100 
+        {
+            name: '21 July', Cases: 1025, pv: 100, amt: 100
         },
-        { 
-            name: '22 July', Cases: 1179, pv: 100, amt: 100 
+        {
+            name: '22 July', Cases: 1179, pv: 100, amt: 100
         },
-        { 
-            name: '23 July', Cases: 1301, pv: 100, amt: 100 
+        {
+            name: '23 July', Cases: 1301, pv: 100, amt: 100
         },
-        { 
-            name: '24 July', Cases: 1422, pv: 100, amt: 100 
+        {
+            name: '24 July', Cases: 1422, pv: 100, amt: 100
         },
-        { 
-            name: '25 July', Cases: 1547, pv: 100, amt: 100 
+        {
+            name: '25 July', Cases: 1547, pv: 100, amt: 100
         },
-        { 
-            name: '26 July', Cases: 1672, pv: 100, amt: 100 
+        {
+            name: '26 July', Cases: 1672, pv: 100, amt: 100
         },
-        { 
-            name: '27 July', Cases: 1779, pv: 100, amt: 100 
+        {
+            name: '27 July', Cases: 1779, pv: 100, amt: 100
         },
-        { 
-            name: '28 July', Cases: 1889, pv: 100, amt: 100 
+        {
+            name: '28 July', Cases: 1889, pv: 100, amt: 100
         },
-        { 
-            name: '29 July', Cases: 2006, pv: 100, amt: 100 
+        {
+            name: '29 July', Cases: 2006, pv: 100, amt: 100
         },
-        { 
-            name: '30 July', Cases: 2006, pv: 100, amt: 100 
+        {
+            name: '30 July', Cases: 2006, pv: 100, amt: 100
         },
     ];
 
 
+    useEffect(() => {
+        const random1 = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
+        const random2 = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
+        const random3 = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
+        const random4 = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
+        setDoses1(random1)
+        setDoses2(random2)
+        setDoses3(random3)
+        setDoses4(random4)
+    }, [])
 
 
     return (
@@ -142,19 +157,19 @@ export default function Dashboard() {
                                                         <hr class="solid"></hr>
                                                         <p>North</p>
                                                         <div className="dashboard-numbercluster">
-                                                            <h1>000</h1>
+                                                            <h1>{doses1}</h1>
                                                         </div>
                                                         <p>East</p>
                                                         <div className="dashboard-numbercluster">
-                                                            <h1>000</h1>
+                                                            <h1>{doses2}</h1>
                                                         </div>
                                                         <p>Central</p>
                                                         <div className="dashboard-numbercluster">
-                                                            <h1>000</h1>
+                                                            <h1>{doses3}</h1>
                                                         </div>
                                                         <p>West</p>
                                                         <div className="dashboard-numbercluster">
-                                                            <h1>000</h1>
+                                                            <h1>{doses4}</h1>
                                                         </div>
                                                     </Card.Body>
                                                 </Card>
@@ -169,13 +184,13 @@ export default function Dashboard() {
                                                         <Card.Title>Submit/Request Doses</Card.Title>
                                                         <hr class="solid"></hr>
                                                         <div>
-                                                            <Link to="/request/:username">
+                                                            <Link to={"/request/" + UN}>
                                                                 <Button className="dashboard-buttons" variant="outline-secondary"> Request Doses </Button>
                                                             </Link>
 
                                                         </div>
                                                         <div>
-                                                            <Link to="/submit/:username">
+                                                            <Link to={"/submit/" + UN}>
                                                                 <Button className="dashboard-buttons" variant="outline-secondary"> Submit Doses </Button>
                                                             </Link>
 
@@ -192,7 +207,7 @@ export default function Dashboard() {
                                                             <Line type="monotone" dataKey="Cases" stroke="#8884d8" />
                                                             <CartesianGrid stroke="#ccc" strokeDasharray="2 2" />
                                                             <XAxis dataKey="name" />
-                                                            <YAxis/>
+                                                            <YAxis />
                                                             <Tooltip />
                                                         </LineChart>
                                                         {/* <img src={FakeChart} className="dashboard-fakeChart" /> */}
@@ -200,23 +215,40 @@ export default function Dashboard() {
                                                 </Card>
                                             </Col>
                                             <Col>
-                                                <Card className="CardShadow">
+                                                <Card className="CardShadow dashboard-newsCard">
                                                     <Card.Body>
                                                         <Card.Title>Covid News for Today</Card.Title>
                                                         <hr class="solid"></hr>
+
                                                         <div>
-                                                            <p><b>Mandatory COVID-19 tests for all students, staff at Punggol Primary School after cases detected</b></p>
-                                                            <p>an hour ago</p>
+                                                            <p><b>European Union pulls ahead of US in Covid-19 vaccinations</b></p>
+                                                            <p>34 min ago</p>
                                                         </div>
                                                         <hr class="solid"></hr>
                                                         <div>
-                                                            <p><b>130 new locally transmitted COVID-19 cases in Singapore; 5 new clusters</b></p>
-                                                            <p>8 hours ago</p>
+                                                            <p><b>Sydney under strict new Covid-19 lockdown rules as cases soar</b></p>
+                                                            <p>58 min ago</p>
                                                         </div>
                                                         <hr class="solid"></hr>
                                                         <div>
-                                                            <p><b>Hospital beds could fill up in a week if a COVID-19 cluster grows 'uncontrollably': Ong Ye Kung</b></p>
-                                                            <p>15 hours ago</p>
+                                                            <p><b>Japan proposes adding four regions to Covid-19 state of emergency</b></p>
+                                                            <p>1 hour ago</p>
+                                                        </div>
+                                                        <hr class="solid"></hr>
+                                                        <div>
+                                                            <p><b>Mexico Covid-19 deaths 35% more than reported: Statistics agency</b></p>
+                                                            <p>1 hour ago</p>
+                                                        </div>
+                                                        <hr class="solid"></hr>
+                                                        <div>
+                                                            <p><b>Vaccinated Americans are getting angry at Covid-19 vaccine holdouts</b></p>
+                                                            <p>2 hours ago</p>
+                                                        </div>
+                                                        <hr class="solid"></hr>
+
+                                                        <div>
+                                                            <p><b>Thai streets empty as India traffic jams return, Apple data show</b></p>
+                                                            <p>3 hours ago</p>
                                                         </div>
 
 
--- savevaccinesg/src/pages/requestPage.js
+++ savevaccinesg/src/pages/requestPage.js
@@ -11,149 +11,10 @@ import config from "../config";
 const BASE_URL = config.BASE_URL
 
 
-// export default function RequestPage() {
-
-//     const [data1, setdata1] = useState()
-//     const [data, setData] = useState([])
-//     const [count, setCount] = useState(0)
-//     const [addremove, setAddRemove] = useState(true)
-
-
-//     useEffect(() => {
-//         // async function fetchData() {
-//         //     const response = await axios.get(BASE_URL + "/clinics")
-//         //     setData(response.data._embedded.clinics)
-//         // }fetchData()
-
-//         const fetchData = async () => {
-//             const response = await await axios.get(BASE_URL + "/clinics")
-//             setData(response.data._embedded.clinics)
-//         }
-//         fetchData()
-//     }, [])
-
-
-//     function addonClick(addcount){
-//         setCount(count + addcount)
-
-//     }
-
-//     return (
-//         <div>
-//             <Row>
-//                 <Col xs={1}><MainNavbar /></Col>
-//                 <Col>
-//                     <div className="request-mainface">
-//                         <Row>
-//                             <Col xs={2}></Col>
-//                             <Col>
-//                                 {/* TITLE */}
-//                                 <div className="request-title">
-//                                     <p>Request Doses</p>
-//                                 </div>
-//                                 {/* CLINIC LIST */}
-//                                 <div className="request-body">
-//                                     <Card className="CardShadow request-list-Card">
-//                                         <Card.Body>
-//                                             <div className="request-table">
-//                                                 <Table striped bordered hover size="sm">
-//                                                     <thead>
-//                                                         <tr>
-//                                                             <th>Name</th>
-//                                                             <th>Address</th>
-//                                                             <th>Vaccine Type</th>
-//                                                             <th>Area</th>
-//                                                             <th>Doses Available</th>
-//                                                             <th></th>
-//                                                         </tr>
-//                                                     </thead>
-//                                                     <tbody>
-//                                                         {/* {data.map(p => (
-//                                                             <tr>
-//                                                                 <td>{p.name}</td>
-//                                                                 <td>{p.address}</td>
-//                                                                 <td>{p.type}</td>
-//                                                                 <td>{p.region}</td>
-//                                                                 <td>{p.dosecount}</td>
-//                                                                 <td>
-//                                                                     <Row>
-//                                                                         <Col>
-//                                                                             <Button onClick={addonClick(p.dosecount)}>Add</Button>
-//                                                                             <Button >Remove</Button>
-//                                                                         </Col>
-//                                                                     </Row>
-//                                                                 </td>
-//                                                             </tr>
-//                                                         ))} */}
-//                                                     </tbody>
-//                                                 </Table>
-//                                             </div>
-//                                         </Card.Body>
-//                                     </Card>
-//                                 </div>
-
-
-
-//                                 {/* TOTAL FINAL */}
-//                                 <div className="request-submit">
-//                                     <Container>
-//                                         <Row>
-//                                             {/* EMPTY COL */}
-//                                             <Col></Col>
-//                                             {/* TOTAL SELECTED COL */}
-//                                             <Col md={3}>
-//                                                 <div>
-//                                                     <Form>
-//                                                         <Form.Group >
-//                                                             <Form.Label>Total Dose Selected</Form.Label>
-//                                                             <Form.Control disabled size="lg" value={count}/>
-//                                                         </Form.Group>
-//                                                     </Form>
-//                                                 </div>
-//                                                 <div className="request-submit-button">
-//                                                     <Button>Submit Request</Button>
-//                                                 </div>
-
-//                                             </Col>
-//                                             <Col>
-//                                             </Col>
-//                                         </Row>
-//                                     </Container>
-//                                 </div>
-//                             </Col>
-//                         </Row>
-//                     </div>
-//                 </Col>
-//             </Row>
-
-
-//             <MainFooter />
-//         </div >
-//     )
-// }
-
-
 export default function RequestPage() {
 
     const [posts, setPosts] = useState([])
-    // const [data, setData] = useState([])
     const [count, setCount] = useState(0)
-    const [addremove, setAddRemove] = useState(true)
-
-
-    // useEffect(() => {
-    //     // async function fetchData() {
-    //     //     const response = await axios.get(BASE_URL + "/clinics")
-    //     //     setData(response.data._embedded.clinics)
-    //     // }fetchData()
-
-    //     const fetchData = async () => {
-    //         const response = await await axios.get(BASE_URL + "/clinics")
-    //         setData(response.data._embedded.clinics)
-    //     }
-    //     fetchData()
-    // }, [])
-
 
     function addonClick(addcount) {
         setCount(count + addcount)
@@ -166,8 +27,6 @@ export default function RequestPage() {
     useEffect(() => {
         axios.get('http://178.128.120.182:8080/clinics?page=0&size=300')
             .then(res => {
-                //console.log(res.data._embedded)
-                //console.log(res.data)
                 setPosts(res.data._embedded.clinics)
             })
             .catch(err => {
