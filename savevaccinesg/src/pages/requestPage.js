612620610a6a26170171b40ce9860735d6b28222
Ryan Tan <Ryan_Tan_1998@hotmail.sg>
2021-07-30 09:09:11 +0800

    tweeks to table

--- savevaccinesg/src/pages/requestPage.js
+++ savevaccinesg/src/pages/requestPage.js
@@ -35,7 +35,7 @@ const BASE_URL = config.BASE_URL
 
 //     function addonClick(addcount){
 //         setCount(count + addcount)
-        
+
 //     }
 
 //     return (
@@ -155,13 +155,16 @@ export default function RequestPage() {
     // }, [])
 
 
-    // function addonClick(addcount){
-    //     setCount(count + addcount)
+    function addonClick(addcount) {
+        setCount(count + addcount)
+    }
+    function removeonClick(removeCount) {
+        setCount(count - removeCount)
+    }
 
-    // }
-    
+
     useEffect(() => {
-        axios.get('http://178.128.120.182:8080/clinics')
+        axios.get('http://178.128.120.182:8080/clinics?page=0&size=300')
             .then(res => {
                 //console.log(res.data._embedded)
                 //console.log(res.data)
@@ -199,37 +202,27 @@ export default function RequestPage() {
                                                             <th>Area</th>
                                                             <th>Doses Available</th>
                                                             <th></th>
+                                                            <th></th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
-                                                        {/* {data.map(p => (
-                                                            <tr>
-                                                                <td>{p.name}</td>
-                                                                <td>{p.address}</td>
-                                                                <td>{p.type}</td>
-                                                                <td>{p.region}</td>
-                                                                <td>{p.dosecount}</td>
+
+
+                                                        {posts.map(post => (
+                                                            <tr key={post.id}>
+                                                                <td>{post.name}</td>
+                                                                <td>{post.address}</td>
+                                                                <td>{post.type}</td>
+                                                                <td>{post.region}</td>
+                                                                <td>{post.dosecount}</td>
                                                                 <td>
-                                                                    <Row>
-                                                                        <Col>
-                                                                            <Button onClick={addonClick(p.dosecount)}>Add</Button>
-                                                                            <Button >Remove</Button>
-                                                                        </Col>
-                                                                    </Row>
+                                                                    <Button onClick={() => addonClick(post.dosecount)} variant="success">+</Button>
+                                                                </td>
+                                                                <td>
+                                                                    <Button onClick={() => removeonClick(post.dosecount)} variant="danger">-</Button>
                                                                 </td>
                                                             </tr>
-                                                        ))}  */}
-                                                        
-                                                            {posts.map(post => (
-                                                                <tr key={post.id}>
-                                                                    <td>{post.name}</td>
-                                                                    <td>{post.address}</td>
-                                                                    <td>{post.type}</td>
-                                                                    <td>{post.region}</td>
-                                                                    <td>{post.dosecount}</td>
-
-                                                                </tr>
-                                                            ))}
+                                                        ))}
                                                     </tbody>
                                                 </Table>
                                             </div>
@@ -239,32 +232,32 @@ export default function RequestPage() {
 
 
 
-                                    {/* TOTAL FINAL */}
-                                    <div className="request-submit">
-                                        <Container>
-                                            <Row>
-                                                {/* EMPTY COL */}
-                                                <Col></Col>
-                                                {/* TOTAL SELECTED COL */}
-                                                <Col md={3}>
-                                                    <div>
-                                                        <Form>
-                                                            <Form.Group >
-                                                                <Form.Label>Total Dose Selected</Form.Label>
-                                                                <Form.Control disabled size="lg" value={count} />
-                                                            </Form.Group>
-                                                        </Form>
-                                                    </div>
-                                                    <div className="request-submit-button">
-                                                        <Button>Submit Request</Button>
-                                                    </div>
+                                {/* TOTAL FINAL */}
+                                <div className="request-submit">
+                                    <Container>
+                                        <Row>
+                                            {/* EMPTY COL */}
+                                            <Col></Col>
+                                            {/* TOTAL SELECTED COL */}
+                                            <Col md={3}>
+                                                <div>
+                                                    <Form>
+                                                        <Form.Group >
+                                                            <Form.Label>Total Dose Selected</Form.Label>
+                                                            <Form.Control disabled size="lg" value={count} />
+                                                        </Form.Group>
+                                                    </Form>
+                                                </div>
+                                                <div className="request-submit-button">
+                                                    <Button>Submit Request</Button>
+                                                </div>
 
-                                                </Col>
-                                                <Col>
-                                                </Col>
-                                            </Row>
-                                        </Container>
-                                    </div>
+                                            </Col>
+                                            <Col>
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                </div>
                             </Col>
                         </Row>
                     </div>
@@ -272,7 +265,7 @@ export default function RequestPage() {
             </Row>
 
 
-                <MainFooter />
+            <MainFooter />
         </div>
-            )
+    )
 }
